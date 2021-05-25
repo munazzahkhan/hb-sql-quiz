750bbddeda47573ed55004c792a220da4f3a7e55
Munazzah Hassan Khan <munazzah.hassan@gmail.com>
2021-05-24 21:29:01 -0700

    completed quiz

--- answers.sql
+++ answers.sql
@@ -280,3 +280,90 @@ made by customer 100.
 
 
 SELECT id, status, order_total FROM orders WHERE customer_id = 100;
+
+
+==========
+21
+
+-----
+
+Write a single query that shows the id, status, and order total for all
+orders made by 'pclark74@gmail.com'. Use a subselect to do this.
+
+
+-----
+
+
+SELECT id, status, order_total
+FROM orders
+WHERE customer_id=(SELECT id FROM customers WHERE email='pclark74@gmail.com');
+
+
+==========
+22
+
+-----
+
+Write a query that shows the id, status, and order total for all orders
+made by 'pclark74@gmail.com'. Use a join to do this.
+
+-----
+
+
+SELECT orders.id, status, order_total
+FROM orders
+JOIN customers ON (customers.id=orders.customer_id)
+WHERE email='pclark74@gmail.com';
+
+
+==========
+23
+
+-----
+
+Write a query that shows all columns in the order_items table for order #2725.
+
+-----
+
+
+SELECT *
+FROM order_items
+WHERE order_id = 2725;
+
+
+==========
+24
+
+-----
+
+Write a query that shows the common_name, melon_type, quantity,
+unit_price and total_price for all the melons in order #2725.
+
+-----
+
+
+SELECT Melons.common_name, 
+       Melons.melon_type,
+       Order_items.quantity,
+       Order_items.unit_price, 
+       Order_items.total_price 
+FROM Order_items 
+  JOIN Melons 
+    ON Order_items.melon_id = Melons.id 
+WHERE order_items.order_id = 2725;
+
+
+==========
+25
+
+-----
+
+Write a query that shows the total amount of revenue that comes from
+internet orders.
+
+-----
+
+
+SELECT SUM(order_total)
+FROM orders 
+WHERE salesperson_id is NULL;
